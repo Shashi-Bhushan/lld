33882eabd1731dc5f3c9ef688960861e9a5d263e
Shashi <Shashi-Bhushan@users.noreply.github.com>
2021-08-17 00:20:15 +0530

    Minor change in main function

--- ticketbooking/src/main/java/in/shabhushan/ticketbooking/TicketBookingApplication.java
+++ ticketbooking/src/main/java/in/shabhushan/ticketbooking/TicketBookingApplication.java
@@ -1,14 +1,7 @@
 package in.shabhushan.ticketbooking;
 
-import in.shabhushan.ticketbooking.enums.Language;
-import in.shabhushan.ticketbooking.models.Movie;
-import in.shabhushan.ticketbooking.repository.MoviesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Date;
-import java.util.HashSet;
 
 @SpringBootApplication
 public class TicketBookingApplication {
@@ -19,8 +12,6 @@ public class TicketBookingApplication {
 
 	public void run(String... args) {
 		System.out.println("Starting application");
-
-
 	}
 
 }
